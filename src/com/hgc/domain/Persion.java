c151e862d1e03c722d54576d610921c3a1e7f80e
hgc <1056123843@qq.com>
2020-11-10 17:15:02 +0800

    123

---
+++ src/com/hgc/domain/Persion.java
@@ -0,0 +1,15 @@
+package com.hgc.domain;/**
+ * ClassName:Persion
+ * Package:com.hgc.domain
+ * description:
+ *
+ * @Date:2020/11/10 17:10
+ * @aUTHOR:HGC
+ */
+
+/**
+ *ClassName:Persion
+ */
+public class Persion {
+    private String id;
+}
